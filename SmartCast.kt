889ec2bc31faef16502800cc43284f58a6e46d80
Anbyeonghun <abh0920one@gmail.com>
2020-11-23 23:14:28 +0900

    201123

--- SmartCast.kt
+++ SmartCast.kt
@@ -1,22 +1,47 @@
 package SmartCast
 
-import java.lang.IllegalArgumentException
+import kotlin.IllegalArgumentException
 
 interface Expr
 class Num(val value: Int) : Expr
 class Sum(val left: Expr, val right: Expr) : Expr
 
-fun eval(e: Expr): Int {
-    if(e is Num){
-        val n = e as Num
-        return n.value
+// if 중첩
+//fun eval(e: Expr): Int {
+//    if(e is Num){
+//        val n = e as Num
+//        return n.value
+//    }
+//    if(e is Sum){
+//        return eval(e.right) + eval(e.left)
+//    }
+//    throw IllegalArgumentException("Unknown expression")
+//}
+
+// if 중첩 대신 when 사용
+fun eval(e: Expr) : Int = when(e){
+    is Num -> e.value
+    is Sum -> eval(e.right) + eval(e.left)
+    else -> throw IllegalArgumentException("Unknown expression")
+}
+
+// 분기에서 블록 사용
+fun evalWithLogging(e: Expr): Int = when(e) {
+    is Num -> {
+        println("num: $(e.value)")
+        e.value
     }
-    if(e is Sum){
-        return eval(e.right) + eval(e.left)
+    is Sum -> {
+        val left = evalWithLogging(e.left)
+        val right = evalWithLogging(e.right)
+        println("sum: $left + $right")
+        left + right
     }
-    throw IllegalArgumentException("Unknown expression")
+    else -> throw IllegalArgumentException("Unknown expression")
 }
 
 fun main() {
     println(eval(Sum(Sum(Num(1), Num(2)), Num(4))))
+    println()
+    println(evalWithLogging(Sum(Sum(Num(1), Num(2)), Num(4))))
 }
